fc4a091e58ed486991d205cf84c21882d41564b5
Daniel <71588528+ErDanieh@users.noreply.github.com>
2023-02-23 12:03:03 +0100

    Discord

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/ErDanieh/WIKI.IMCR.git',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/JnydXrAB',
   },
   docsRepositoryBase: 'https://github.com/ErDanieh/WIKI.IMCR.git',
   footer: {
